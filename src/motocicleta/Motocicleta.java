777914d8455d20f33dbbba2642d06468631d91c9
ricardolecocq <ricardolecocq@hotmail.com>
2015-04-16 15:32:31 -0300

    Carro e relativos por Joao, Motos e relativos por Eduardo, Loja por Ricardo

---
+++ src/motocicleta/Motocicleta.java
@@ -0,0 +1,23 @@
+package motocicleta;
+
+public class Motocicleta
+{
+	Cor cor;
+	Cilindrada cilindrada;
+	CapTanque capTanque;
+	Modelo modelo;
+	Montadora montadora;
+	Tipo tipo;
+	
+	public Motocicleta(Cor _cor,Cilindrada _cilindrada,CapTanque _capTanque,Modelo _modelo,Montadora _montadora,Tipo _tipo)
+	{
+		cor = _cor;
+		cilindrada = _cilindrada;
+		capTanque = _capTanque;
+		modelo = _modelo;
+		tipo = _tipo;
+		
+		
+	}
+
+}
