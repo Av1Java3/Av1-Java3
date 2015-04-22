d97e885305bea815dfb9ff151fd0c65e584e4af8
ricardo.mferreira <ricardolecocq@hotmail.com>
2015-04-22 11:50:34 -0300

    Criação método Add Carro

--- src/classes/Loja.java
+++ src/classes/Loja.java
@@ -3,7 +3,13 @@ package classes;
 import java.util.ArrayList;
 
 import motocicleta.Motocicleta;
+import carro.Cambio;
 import carro.Carro;
+import carro.Cor;
+import carro.Modelo;
+import carro.Montadora;
+import carro.Motorizacao;
+import carro.Tipo;
 
 public class Loja
 {
@@ -17,7 +23,17 @@ public class Loja
 	
 	public void adicionarCarro()
 	{
-		
+		Cambio _cambio = null;
+		Cor _cor = null;
+		Modelo _modelo = null;
+		Montadora _montadora = null;
+		Tipo _tipo = null;
+		Motorizacao _motorizacao = null; 
+		String _chassi = "";
+		float _preco = 0;
+	
+		Carro aux = new Carro(_chassi, _preco, _montadora, _modelo, _motorizacao, _cambio, _tipo, _cor);
+		estoqueCarros.add(aux);
 	}
 	
 		
